25174f8bc6ce5c9ff175a627ed209eb5d4e5d324
Zhang_Leran <A1908721346@gmail.com>
2025-01-23 23:06:11 +0800

    Rename main, add skeletal version of greet and bye

---
+++ src/main/java/Orim.java
@@ -0,0 +1,13 @@
+public class Orim {
+    public static void main(String[] args) {
+        String LINE  = "____________________________________________________________\n";
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo + LINE);
+        System.out.println("Hello! I'm Orim.\n" + "What can I do for you?\n" + LINE);
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
