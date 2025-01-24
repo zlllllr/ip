8adc08f5bdae87bd01ec4f2090f55a6131a3580b
Zhang_Leran <A1908721346@gmail.com>
2025-01-24 10:28:05 +0800

    add support to store

--- src/main/java/Orim.java
+++ src/main/java/Orim.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Orim {
@@ -8,16 +9,28 @@ public class Orim {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+        String[] taskList = new String[101];
+        int numOfTask = 1;
         Scanner sc = new Scanner(System.in);
         System.out.println("Hello from\n" + logo);
         System.out.println(LINE + "Hello! I'm Orim.\n" + "What can I do for you?\n" + LINE);
         while (sc.hasNext()) {
-            String temp = sc.next();
-            if (temp.equals("bye")){
-                System.out.println(LINE + "Bye. Hope to see you again soon!" + LINE);
+            String temp = sc.nextLine();
+            if (temp.equals("bye")) {
+                System.out.println(LINE + "Bye. Hope to see you again soon!\n" + LINE);
                 System.exit(0);
             }
-            System.out.println(LINE + temp + "\n" + LINE);
+            if (temp.equals("list")) {
+                System.out.println(LINE);
+                for (int i = 1; i < numOfTask; i++) {
+                    System.out.println(i + ". " + taskList[i] + "\n");
+                }
+                System.out.println(LINE);
+                continue;
+            }
+            taskList[numOfTask] = temp;
+            numOfTask++;
+            System.out.println(LINE + "added: " + temp + "\n" + LINE);
         }
     }
 }
